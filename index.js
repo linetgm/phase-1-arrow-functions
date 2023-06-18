cd6ee1ab9e09d1edb2fae10d33f1000f0ccaadfd
Linet <muthiigathoni@gmail.com>
2023-06-19 01:30:07 +0300

    Complete assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+let num1 = 2000;
+let num2 = 100
+const divide = function(){
+    return num1/num2;
+}
+divide();
+
+const square = x => x * x;
+square(2);
+
+const add = (a, b) => a + b;
+add(3,4);
